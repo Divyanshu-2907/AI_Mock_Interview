fc33ab9cb3a2d561d31f1312fb5d2f0a6d72cc4d
Divyanshu Kumar <130997107+Divyanshu-2907@users.noreply.github.com>
2025-06-14 12:26:52 +0530

    Initial commit: AI Mock Interview application with Firebase integration

---
+++ firebase/client.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmVhWZo867m5g9lAo5dk0sFbFFv1X-Hzc",
+  authDomain: "aicruit-783cf.firebaseapp.com",
+  projectId: "aicruit-783cf",
+  storageBucket: "aicruit-783cf.firebasestorage.app",
+  messagingSenderId: "499639912941",
+  appId: "1:499639912941:web:2c6475a7c14aa08efdffe4",
+  measurementId: "G-YLHTYKTD65"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
